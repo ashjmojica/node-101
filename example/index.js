2b0b8f3e52deea46e2d289b5f7a0ad49a989e4d1
Ashley Mojica <ashjmojica@gmail.com>
2023-05-16 07:38:37 -0500

    complete

--- example/index.js
+++ example/index.js
@@ -6,3 +6,16 @@ const cowsay = require("cowsay");
 
 console.log(cowsay.say({ text: "I'm a module" }));
 app.listen(PORT, console.log(`listening on port ${PORT}`));
+
+
+app.get("/cowsay", (req, res) => {
+  res.send(
+    cowsay.say({
+      text: "I'm a moooodule",
+      e: "oO",
+      T: "U ",
+    })
+  );
+});
+
+app.listen(PORT, console.log(`listening on port ${PORT}`));
